840bf72e57936792df10812612d704ca1f7a11c7
MaylaSilva <may.siqueira93@gmail.com>
2022-10-25 17:14:29 -0300

    Primeiro Commit

---
+++ src/Midia.java
@@ -0,0 +1,39 @@
+abstract class Midia {
+    private String codigo;
+    private Double preco;
+    private String nome;
+
+    public Midia(String codigo, Double preco, String nome) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(Double preco){
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void darPlay (){
+        System.out.println("Foi dado play");
+    }
+}
